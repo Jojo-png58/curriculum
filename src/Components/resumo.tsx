6524071c32e0a238326c72291182eb4d10d81fc3
Jojo-png58 <jonatasouza.social@outlook.com>
2023-04-24 20:41:04 -0300

    Update for resumopage

--- src/Components/resumo.tsx
+++ src/Components/resumo.tsx
@@ -23,8 +23,8 @@ export function Resumo(){
             <div className='grid items-center gap-4 mt-4 bg-zinc-800 p-4 rounded-2xl'>
                 <h1 className='font-bold text-2xl'>Experiências Profissionais</h1>
                 <div className='font-semibold text-lg'>CIMATEC jr - Empresa júnior do SENAI CIMATEC (2020-2022)</div>
-                <div>- Cargo: Coordenador de projetos (2020-2021) - Atuei em 5 projetos com uma equipe enxuta de 4 pessoas, aplicando 5S e fazendo pesquisas de mercado, o faturamento total dos projetos foi de 7K com NPS 98.</div>
                 <div>- Cargo: Diretor de Gente e Gestão (2021-2022) - Compus o quadro societário da empresa na gestão de 2022, gerindo um departamento de 10 pessoas, atuando principalmente na gestão de pessoas e financeiro, realizamos um planejamento estratégico que nos permitiu alcançar a marca de 360K de faturamento e 60 projetos com NPS 90.</div>
+                <div>- Cargo: Coordenador de projetos (2020-2021) - Atuei em 5 projetos com uma equipe enxuta de 4 pessoas, aplicando 5S e fazendo pesquisas de mercado, o faturamento total dos projetos foi de 7K com NPS 98.</div>
             </div>
             <div className='grid items-center gap-4 mt-4 bg-zinc-800 p-4 rounded-2xl'>
                 <h1 className='font-bold text-2xl'>Competências</h1>
